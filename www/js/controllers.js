ca74bea3c4bd35c969e4fbd65e4238ce6c7c2434
TeerasakGame <manu98765@hotmail.com>
2016-08-31 11:40:39 +0700

    แก้ไขหน้า timeline

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -213,34 +213,42 @@ angular.module('starter.controllers', [])
  .controller('CropTimelineCtrl',function($scope){
    $scope.timeline = [{
       date: new Date(),
-      title: "I am here",
-      author: "Ludo Anderson",
-      profilePicture: "https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg",
-      text: "Lorem ipsum dolor sit amet",
-      type: "location"
+      title: "การเตรียมดิน",
+      author: "วันที่ 1 ส.ค.59 - 10 ส.ค.59",
+      profilePicture: "img/chaokaset_logo.png",
+      Picture:"true",
+      text: "รายละเอียด",
+      a:'0',
+      type: "picture"
 
     }, {
       date: new Date(),
-      title: "For my friends",
-      author: "Sara Orwell",
-      profilePicture: "https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      type: "text"
-
-    }, {
-      date: new Date(),
-      title: "Look at my video!",
-      author: "Miranda Smith",
-      profilePicture: "https://static.licdn.com/scds/common/u/images/apps/plato/home/photo_profile_headshot_200x200_v2.jpg",
-      text: "Lorem ipsum dolor sit amet",
+      title: "กู้เงิน",
+      author: "วันที่ 2 ส.ค.59",
+      profilePicture: "",
+      Picture:"false",
+      text: "รายละเอียด",
+      a:'1',
       type: "video"
 
     }, {
       date: new Date(),
-      title: "Awesome picture",
-      author: "John Mybeweeg",
-      profilePicture: "http://www.lawyersweekly.com.au/images/LW_Media_Library/LW-602-p24-partner-profile.jpg",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      title: "เพลี้ยลง",
+      author: "วันที่ 10 ส.ค.59",
+      profilePicture: "img/chaokaset_logo.png",
+      Picture:"true",
+      text: "รายละเอียด",
+      a:'1',
+      type: "location"
+
+    }, {
+      date: new Date(),
+      title: "เอาน้ำเข้านา",
+      author: "วันที่ 12 ส.ค.59",
+      profilePicture: "img/chaokaset_logo.png",
+      Picture:"5555",
+      text: "รายละเอียด",
+      a:'1',
       type: "picture"
     }]
 
